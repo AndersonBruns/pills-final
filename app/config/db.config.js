46ff5eed7f73ced3dc215b4c841ea53c5fb15d62
Anderson Bruns <30638794+AndersonBruns@users.noreply.github.com>
2021-10-20 19:08:04 -0300

    heroku-db-set

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "root",
-  DB: "pills",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+  HOST: "us-cdbr-east-04.cleardb.com",
+  USER: "b6499fe3dd63a6",
+  PASSWORD: "e2843d03",
+  DB: "heroku_04a381fdd3ebe66"
 };
