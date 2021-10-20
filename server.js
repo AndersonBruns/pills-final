f101486781957d4f87193e8c312e1f2d6c216b26
Anderson Bruns <30638794+AndersonBruns@users.noreply.github.com>
2021-10-20 19:44:13 -0300

    issue-db

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
-app.use(express.json());  /* bodyParser.json() is deprecated */
+app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 
@@ -32,7 +30,7 @@ app.get("/", (req, res) => {
 require("./app/routes/pills.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
