db3ba6d858fb96027e677dd5e3b05f3b6377f355
Anderson Bruns <30638794+AndersonBruns@users.noreply.github.com>
2021-10-20 19:52:14 -0300

    change-db-pool

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,5 +2,12 @@ module.exports = {
   HOST: "us-cdbr-east-04.cleardb.com",
   USER: "b6499fe3dd63a6",
   PASSWORD: "e2843d03",
-  DB: "heroku_04a381fdd3ebe66"
+  DB: "heroku_04a381fdd3ebe66",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
