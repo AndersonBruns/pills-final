212275e34b6cdb52ab6cc4d11e3972152452f1ad
Anderson Bruns <30638794+AndersonBruns@users.noreply.github.com>
2021-11-04 19:57:48 -0300

    commit-finish

--- app/controllers/pills.controller.js
+++ app/controllers/pills.controller.js
@@ -51,7 +51,7 @@ exports.returnEsp = (req, res) => {
     const jsonData = JSON.parse(JSON.stringify(data))
     let ret = "Não"
     for (var i = 0; i < jsonData.length; i++) {
-      let dataFinal = jsonData[i].data.substr(0, 11) + jsonData[i].hora+3
+      let dataFinal = jsonData[i].data.substr(0, 11) + jsonData[i].hora
       let dataPills = new Date(dataFinal)
       let dataAtual = new Date()
       dataPills.setSeconds(0)
